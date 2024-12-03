d380965bf8cd2f90b3933a12f5573505a39818fc
Assy20-bot2 <adembele7744@gmail.com>
2024-12-03 14:09:57 +0000

    Add files via upload
    this project is a simple example of Hibernate`s using with Spring boot framework

---
+++ hiber/src/main/java/com/fst/hiber/model/User1.java
@@ -0,0 +1,24 @@
+package com.fst.hiber.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+@Data
+@Entity
+@Table(name = "users")
+public class User1 {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private boolean enable;
+
+}
---
+++ hiber/src/main/java/com/fst/hiber/repository/User1Reposository.java
@@ -0,0 +1,9 @@
+package com.fst.hiber.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fst.hiber.model.User1;
+
+public interface User1Reposository extends JpaRepository<User1,Long>{
+
+}
